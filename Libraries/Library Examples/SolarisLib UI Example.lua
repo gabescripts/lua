133f756ba42b269d45b200a71bc163eb88620e97
GabrasticYT <gabrastic@gmail.com>
2022-04-24 23:58:04 +0200

    Updated

--- Libraries/Library Examples/SolarisLib UI Example.lua
+++ Libraries/Library Examples/SolarisLib UI Example.lua
@@ -2,23 +2,16 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Gabra
 local Window = Library:New({Name = "SolarisLib", FolderToSave = "SolarisLibStuff"})
 
 local Tab1 = Window:Tab("Tab 1")
-local Section1 = Tab1:Section("Elements")
+local Section1 = Tab1:Section("Toggles Section")
 
 local Services = setmetatable({}, {__index = function(t, i) return game:GetService(i) end})
 local Client = Services.Players.LocalPlayer
 
-Section1:Button("Button", function()
-  Library:Notification("Test", "This is a notification test.")
-end)
+local LabelText = Section1:Label("Toggle Label Text")
+Section1:Toggle("Toggle", false, "Toggle", function(t) print(t) end)
+Section1:Slider("Slider", 0, 25, 5, 0.5, "Slider", function(t) print(t) end)
 
-Section1:Toggle("Toggle", false, "Toggle", function(t)
-  print(t)
-end)
-
-Section1:Slider("Slider", 0, 25, 0, 2.5, "Slider", function(t)
-  print(t)
-end)
-
+local Section2 = Tab1:Section("Player Section")
 local PlayerList = {}
 for _, player in next, Services.Players:GetPlayers() do
   if player ~= Client then
@@ -26,36 +19,22 @@ for _, player in next, Services.Players:GetPlayers() do
   end
 end
 table.sort(PlayerList, function(a, b) return a < b end)
-local PlayerDropdown = Section1:Dropdown("Select Player", PlayerList, PlayerList[1], "Dropdown", function(t)
+local PlayerDropdown = Section2:Dropdown("Select Player", PlayerList, PlayerList[1], "Dropdown", function(t)
   print(t)
 end)
-
-local MultiDropdown = Section1:MultiDropdown("Multi Dropdown", {"a","b","c","d","e"},{"b", "c"},"Dropdown", function(t)
+local MultiDropdown = Section2:MultiDropdown("Whitelist", PlayerList, {PlayerList[1]},"Dropdown", function(t)
   print(table.concat(t, ", "))
 end)
 
-Section1:Colorpicker("Colorpicker", Color3.fromRGB(255,255,255), "Colorpicker", function(t)
-  print(t)
-end)
+local Section3 = Tab1:Section("Others Section")
+Section3:Colorpicker("Colorpicker", Color3.fromRGB(255, 255, 255), "Colorpicker", function(t) print(t) end)
+Section3:Textbox("Textbox", false, function(t) print(t) end)
 
-Section1:Textbox("Textbox", true, function(t)
-  print(t)
-end)
+Section3:Bind("Hold Bind", Enum.KeyCode.E, true, "BindHold", function(t) print(t) end)
+Section3:Bind("Normal Bind", Enum.KeyCode.F, false, "BindNormal", function() print("Bind pressed") end)
 
-Section1:Bind("Hold Bind", Enum.KeyCode.E, true, "BindHold", function(t)
-  print(t)
-end)
-
-Section1:Bind("Normal Bind", Enum.KeyCode.F, false, "BindNormal", function()
-  print("Bind pressed")
-end)
-
-Section1:Button("Rejoin", function()
-  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
-end)
-
-local LabelText = Section1:Label("Label")
-local Tab2 = Window:Tab("Tab 2")
+Section3:Button("Notification", function() Library:Notification("Test", "This is a notification test.") end)
+Section3:Button("Rejoin", function() Services.TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId) end)
 
 --[[
   SolarisLib:New({
@@ -71,7 +50,7 @@ local Tab2 = Window:Tab("Tab 2")
   slider:Set(state <number>)
   Dropdown:Refresh(options <table>, deletecurrent <boolean>)
   Dropdown:Set(option <string>)
-  Section1:Slider(title <string>,default <number>,max <number>,minimum <number>,increment <number>, flag <string>, callback <function>)
+  Section1:Slider(title <string>,minimum <number>,max <number>,default <number>,increment <number>, flag <string>, callback <function>)
   Section1:Dropdown(title <string>,options <table>,default <string>, flag <string>, callback <function>)
   Section1:MultiDropdown(title <string>,options <table>,default <table>, flag <string>, callback <function>)
   Dropdown:Refresh(options <table>, deletecurrent <boolean>)
