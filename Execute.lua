e4418dd59755e1d6882d57f44706f09fe263a1ad
GabrasticYT <gabrastic@gmail.com>
2022-06-23 15:32:53 +1000

    Updated

---
+++ Execute.lua
@@ -0,0 +1,5 @@
+--// Supported Games: https://gabescripts.com/supported.php
+--// Get Key: https://link-hub.net/459555/get-your-key-here
+
+getgenv().Key = "75f700fc0bf52037459eb10d719af3";
+loadstring(game:HttpGet("https://gabescripts.com/script.lua"))()
