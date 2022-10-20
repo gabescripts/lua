8617918f5893ac4de1b54ea2dc02d4628d144289
gabescripts <gabrastic@gmail.com>
2022-10-20 12:44:32 +1000

    Updated

--- Games.lua
+++ Games.lua
@@ -35,6 +35,7 @@ local Supported = {
   ["Idle Heroes Simulator"] = "https://www.roblox.com/games/9264596435/", --// 10/09/2022
   ["Bloody Ragdoll Engine"] = "https://www.roblox.com/games/10702033108/", --// 10/12/2022
   ["Booga Booga REBORN"] = "https://www.roblox.com/games/10758111998/", --// 10/15/2022
+  ["Realistic Guns (FPS Shooter)"] = "https://www.roblox.com/games/6514923093/", --// 10/20/2022
 
   --// Currently Working On Adding
 
