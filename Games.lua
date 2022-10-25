c0d73f152da09b8910d597fff68bc0af8586f071
gabescripts <gabrastic@gmail.com>
2022-10-26 01:57:56 +1000

    Updated

--- Games.lua
+++ Games.lua
@@ -2,7 +2,7 @@
 
 local Supported = {
   
-  --// Currently Supports 33 Games
+  --// Currently Supports 34 Games
  
   ["Rate My Avatar"] = "https://www.roblox.com/games/6708206173/", --// 08/13/2022
   ["Fling Things and People"] = "https://www.roblox.com/games/6961824067/", --// 10/15/2022
@@ -37,8 +37,8 @@ local Supported = {
   ["Booga Booga REBORN"] = "https://www.roblox.com/games/10758111998/", --// 10/25/2022
   ["Realistic Guns (FPS Shooter)"] = "https://www.roblox.com/games/6514923093/", --// 10/20/2022
   ["Grapple People Simulator"] = "https://www.roblox.com/games/6338482540/", --// 10/25/2022
-  ["Aimstars FPS AIM TRAINER"] = "https://www.roblox.com/games/5153937061/", --// 10/25/2022
-  
+  ["Aimstars FPS AIM TRAINER"] = "https://www.roblox.com/games/5153937061/", --// 10/26/2022
+
   --// Currently Working On Adding
 
   ["Infectious Smile"] = "https://www.roblox.com/games/5985232436/", --// 90% Complete
