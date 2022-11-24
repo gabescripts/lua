cefdc229facc48894472aa127a8016138e5922e2
gabescripts <gabrastic@gmail.com>
2022-11-25 03:21:53 +1000

    Updated

--- Games.lua
+++ Games.lua
@@ -22,6 +22,7 @@ local Supported = {
   ["Realistic Guns (FPS Shooter)"] = "https://www.roblox.com/games/6514923093/", --// 10/20/2022
   ["The Rake REMASTERED"] = "https://www.roblox.com/games/2413927524/", --// 10/13/2022
   ["CHAOS"] = "https://www.roblox.com/games/6441847031", --// 10/12/2022
+  
   ["Idle Heroes Simulator"] = "https://www.roblox.com/games/9264596435/", --// 10/09/2022
   ["Training Simulator"] = "https://www.roblox.com/games/10047913840/", --// 10/06/2022
   ["Gun Fighting Tycoon"] = "https://www.roblox.com/games/8571871474/", --// 09/30/2022
