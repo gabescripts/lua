beaa7568e61a57d0771d3adb0ad2cfd59a6e6048
gabescripts <gabrastic@gmail.com>
2022-11-26 21:57:24 +1000

    Updated

--- Games.lua
+++ Games.lua
@@ -5,6 +5,7 @@ local Supported = {
   
   --// Currently Supports 38 Games
  
+  ["Booga Booga Modded"] = "https://www.roblox.com/games/11447840296/", --// 11/26/2022
   ["making scam calls to save your best friend tycoon"] = "https://www.roblox.com/games/11330149751/", --// 11/26/2022
   ["Murderers vs. Sheriffs"] = "https://www.roblox.com/games/5154858502/", --// 11/26/2022
   ["Slayer Tycoon"] = "https://www.roblox.com/games/6490016198/" --// 11/24/2022
